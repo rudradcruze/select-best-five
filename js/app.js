b7028e0ee3050dce1d292c1782255ef91cd97429
rudradcruze <francisrudra@gmail.com>
2022-09-14 17:10:11 +0600

    build getValue function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,13 @@ const playerList = document.getElementsByClassName('buttonSelectClick');
 for (const player of playerList) {
     player.addEventListener('click', function (event) {
         const playerName = event.target.parentNode.previousElementSibling.children[0].innerText;
-        appendChild('playerContainer', playerName);
+        const append = appendChild('playerContainer', playerName);
+        if(append === true)
+            event.target.disabled = 'true';
     })
 }
+
+document.getElementById('playerBudgetCalculation').addEventListener('click', function (){
+    const getPlayerCost = getInputValueById('playerBudget');
+    console.log(getPlayerCost);
+})
--- js/utility.js
+++ js/utility.js
@@ -1,13 +1,26 @@
 function appendChild(appendId, playerName) {
-    const playerContainer = document.getElementById(appendId);
-    const playerPerviousCount = playerContainer.childElementCount;
-
+    const playerPerviousCount = playerCount(appendId);
     if(playerPerviousCount >= 5)
     {
         alert("You have already added 5 player! You can't add more.");
-        return;
+        return false;
     }
     const createNewPlayer = document.createElement('li');
     createNewPlayer.innerText = playerName;
     playerContainer.appendChild(createNewPlayer);
+
+    return true;
+}
+
+function playerCount(elementId) {
+    const playerContainer = document.getElementById(elementId);
+    const playerPerviousCount = playerContainer.childElementCount;
+    return playerPerviousCount;
+}
+
+function getInputValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValue = parseInt(element.value);
+    element.value = '';
+    return elementValue;
 }
