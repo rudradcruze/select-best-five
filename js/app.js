a19c79e32620768a33295b184d9feaa4c60b2640
rudradcruze <francisrudra@gmail.com>
2022-09-14 16:17:39 +0600

    Player name extract

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const playerList = document.getElementsByClassName('buttonSelectClick');
+
+for (const player of playerList) {
+    player.addEventListener('click', function (event) {
+        const playerName = event.target.parentNode.previousElementSibling.children[0].innerText;
+    })
+}
